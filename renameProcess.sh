c51c39cbc297e382ca8253be6458fdcecbb2e473
gerg <gerrrrrrrg@gmail.com>
2021-12-11 09:15:02 -0500

    add templates and rename script

---
+++ renameProcess.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "You must give the process a new name!"
+    exit 1
+fi
+
+rm -rf $1
+cp -r TEMPLATE_PROCESS $1
+
+# get the upper level folder (in opt) before tackling anything else
+for file in $(find $1 -name 'TEMPLATE_PROCESS')
+do
+	mv $file $(echo "$file" | sed -r "s|TEMPLATE_PROCESS|$1|g")
+done
+
+# rename nested files
+for file in $(find $1 -name '*TEMPLATE_PROCESS*')
+do
+	mv $file $(echo "$file" | sed -r "s|TEMPLATE_PROCESS|$1|g")
+done
+
+# find and replace all instances of TEMPLATE_PROCESS
+find $1 -type f -name "*" -print0 | xargs -0 sed -i '' -e "s/TEMPLATE_PROCESS/$1/g"
+
+cp pack_deb.sh $1_pack_deb.sh
+cp install_deb.sh $1_install_deb.sh
+find . -type f -name "$1*" -print0 | xargs -0 sed -i '' -e "s/TEMPLATE_PROCESS/$1/g"
